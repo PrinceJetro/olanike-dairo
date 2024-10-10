6e9208e1059b831d4c4ba21b3fdf518a3ace145c
PrinceJetro <adegbuyijephthah@gmail.com>
2024-10-10 11:35:45 +0100

    first review with the cloient

---
+++ src/data/general.js
@@ -0,0 +1,38 @@
+import director from "../images/director.jpg";
+
+
+export default {
+    school_logo:"https://edusko-dev-bucket.s3.eu-north-1.amazonaws.com/sFldnk5cggg8nk_PsJy798Spxw0EMZWU.webp",
+    school_image: "https://edusko-dev-bucket.s3.eu-north-1.amazonaws.com/QC8QXgAwzXnd7LQz7Vm8glGSOOlr9dKt.webp",
+    school_name:"Olanike Dairo",
+    school_moto:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    school_address:"2 Strachan St, Lagos Island, Lagos 102273, Lagos",
+    school_phone :"2348023024062",
+    school_email:"info@schoolname.edu",
+    school_map:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.5673623728176!2d3.4025011740802853!3d6.4495485240202015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8bfce198edcf%3A0xccb491ac10c9828e!2sOlanike%20Dairo%20Children%20Centre%20Nursery%20And%20Primary%20School.!5e0!3m2!1sen!2sng!4v1728556459909!5m2!1sen!2sng",
+    director_image: director,
+    director_speech:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illum delectus, impedit nihil sapiente, ut aut nisi laudantium, culpa neque ad? Autem atque consequatur iure doloribus ratione aliquam nulla id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illum delectus, impedit nihil sapiente, ut aut nisi laudantium, culpa neque ad? Autem atque consequatur iure doloribus ratione aliquam nulla id?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illum delectus, impedit nihil sapiente, ut aut nisi laudantium, culpa neque ad? Autem atque consequatur iure doloribus ratione aliquam nulla id?`
+}
+
+
+
+
+
+
+
+
+// import director from "../images/director.jpg";
+
+
+// export default {
+//     school_logo:"https://img.freepik.com/premium-vector/school-logo-design_706452-12.jpg",
+//     school_image: "https://worldlandscapearchitect.com/wp-content/uploads/2019/09/MAGIST%C3%89RIO-MUTU-YA-KEVELA-Luanda-Angola-12-678x452.jpg",
+//     school_name:"Lorem Ipsum",
+//     school_moto:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+//     school_address:"123 School Lane, Education City",
+//     school_phone :"(123) 456-7890",
+//     school_email:"info@schoolname.edu",
+//     school_map:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15856.49653249075!2d3.360150033100824!3d6.505967015440521!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8c8c358b3f4b%3A0x5552cfbd43df8434!2sYaba%2C%20Oworonshoki%2C%20Lagos!5e0!3m2!1sen!2sng!4v1728420378236!5m2!1sen!2sng",
+//     director_image: director,
+//     director_speech:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illum delectus, impedit nihil sapiente, ut aut nisi laudantium, culpa neque ad? Autem atque consequatur iure doloribus ratione aliquam nulla id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illum delectus, impedit nihil sapiente, ut aut nisi laudantium, culpa neque ad? Autem atque consequatur iure doloribus ratione aliquam nulla id?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illum delectus, impedit nihil sapiente, ut aut nisi laudantium, culpa neque ad? Autem atque consequatur iure doloribus ratione aliquam nulla id?`
+// }
