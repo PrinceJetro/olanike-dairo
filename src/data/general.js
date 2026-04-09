cd0c5c0e549f08f245569bba103e49e64603c462
princejetro <adegbuyijephthah@gmail.com>
2026-04-09 15:20:56 +0100

    Migration: Switching from React to Vanilla HTML/CSS/JS

--- src/data/general.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-    school_name:"Olanike Dairo",
-    school_moto:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    school_address:"2 Strachan St, Obalende, Lagos Island, Lagos 102273, Lagos",
-    school_phone :"02013424038",
-    school_email:"olanikedairocentre@gmail.com",
-    school_map:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.567404067976!2d3.4025011749922878!3d6.449543193541855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8bfce198edcf%3A0xccb491ac10c9828e!2sOlanike%20Dairo%20Children%20Centre%20Nursery%20And%20Primary%20School.!5e0!3m2!1sen!2sng!4v1729082423478!5m2!1sen!2sng",
-}
